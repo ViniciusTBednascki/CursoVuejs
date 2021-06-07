159270516efaa932e96d31bf931e144494cd094e
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-07 17:52:26 -0300

    implementado os metodos de ataque e lidando com o fim de jogo

--- projeto01-MonsterKiller/app.js
+++ projeto01-MonsterKiller/app.js
@@ -19,9 +19,26 @@ new Vue({
         giveUp() {
             this.running = false
             this.playerLife = 0
+        },
+        getRandom(min, max) {
+            const value = Math.random()*(max - min) + min
+            return Math.round(value)
+        },
+        attack(especial) {
+            this.hurt('playerLife', 7, 12, false)
+            this.hurt('monsterLife', 5, 10, especial)
+        },
+        hurt(target, min, max, especial) {
+            const bonus = especial ? 4 : 0
+            const hurt = this.getRandom(min + bonus, max + bonus)
+            this[target] = Math.max(this[target] - hurt, 0)
         }
     },
     watch: {
-
+        hasResult(value) {
+            if(value) {
+                this.running = false
+            }
+        }
     }
 })

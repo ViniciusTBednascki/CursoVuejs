204bdcfcbec6d6f0bd7267825da4157a65ec679e
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-07 19:13:42 -0300

    implementando o metodo de descanso, energia do jogador e furia do monstro

--- projeto01-MonsterKiller/app.js
+++ projeto01-MonsterKiller/app.js
@@ -3,7 +3,10 @@ new Vue({
     data: {
         running: false,
         playerLife: 100,
-        monsterLife: 100
+        playerEnergy: 100,
+        monsterLife: 100,
+        monsterFury: 0,
+        monsterEspecial: false
     },
     computed: {
         hasResult() {
@@ -14,30 +17,62 @@ new Vue({
         startGame() {
             this.running = true
             this.playerLife = 100
+            this.playerEnergy = 100
             this.monsterLife = 100
+            this.monsterFury = 0
         },
         giveUp() {
             this.running = false
             this.playerLife = 0
+            this.playerEnergy = 0
         },
         getRandom(min, max) {
             const value = Math.random()*(max - min) + min
             return Math.round(value)
         },
         attack(especial) {
-            this.hurt('playerLife', 7, 12, false)
             this.hurt('monsterLife', 5, 10, especial)
+            this.hurt('playerLife', 5, 10, this.monsterEspecial)
+            this.monsterFury = Math.min(this.monsterFury+20, 100)
+        },
+        whatHappend(target, especial) {
+            if(especial && this.playerEnergy >= 25 && target == 'monsterLife') {
+                this.playerEnergy = Math.max(this.playerEnergy-25, 0)
+                return 4
+            }else if(especial && target == 'playerLife') {
+                this.monsterFury = 0
+                this.monsterEspecial = false
+                return 8
+            }else if(target == 'monsterLife'){
+                this.playerEnergy = Math.min(this.playerEnergy+10, 100)
+            }
+            return 0
         },
         hurt(target, min, max, especial) {
-            const bonus = especial ? 4 : 0
+            let bonus = this.whatHappend(target, especial)
             const hurt = this.getRandom(min + bonus, max + bonus)
             this[target] = Math.max(this[target] - hurt, 0)
+        },
+        rest(min, max) {
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
+            this.playerEnergy = Math.min(this.playerEnergy + 20, 100)
+        },
+        restAndHurt() {
+            this.rest(5, 10)
+            this.hurt('playerLife', 5, 10, this.monsterEspecial)
+            this.monsterFury = Math.min(this.monsterFury+20, 100)
         }
     },
     watch: {
         hasResult(value) {
             if(value) {
                 this.running = false
+            }
+        },
+        monsterFury(value) {
+            if(value == 100) {
+                this.monsterEspecial = true
             }
         }
     }

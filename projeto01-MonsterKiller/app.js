6feb4cad8d3fda70e065c54d75fbc101d21d657c
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-07 16:44:23 -0300

    criando e lidando com os resultados do jogo

--- projeto01-MonsterKiller/app.js
+++ projeto01-MonsterKiller/app.js
@@ -5,10 +5,12 @@ new Vue({
         monsterLife: 100
     },
     computed: {
-
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
     },
     methods: {
-
+        
     },
     watch: {
 

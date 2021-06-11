695b0cc2b03b2cec27f93fa2d4b8718b246da221
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-11 16:23:57 -0300

    Refatorando as funcionalidades do App em diferentes componentes

---
+++ componentes/componente-desafio/src/article/article.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="article">
+        <article-headline />
+
+        <article-content />
+    </div>
+</template>
+
+<script>
+import articleHeadline from './articleHeadline.vue'
+import articleContent from './articleContent.vue'
+
+export default {
+    components: {
+        'article-headline': articleHeadline,
+        'article-content': articleContent
+    }
+}
+</script>
---
+++ componentes/componente-desafio/src/navigation/navigationDrawer.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-navigation-drawer app>
+      <navigation-toolbar />
+
+      <v-divider></v-divider>
+
+      <navigation-list />
+    </v-navigation-drawer>
+</template>
+
+<script>
+import navigationToolbar from './navigationToolbar.vue'
+import navigationList from './navigationList.vue'
+export default {
+  components: {
+    'navigation-toolbar': navigationToolbar,
+    'navigation-list': navigationList
+  }
+}
+</script>

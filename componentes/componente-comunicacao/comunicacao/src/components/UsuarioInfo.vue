56e532b6acef392ee76ec047b2660a6eb2e34066
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-14 15:52:56 -0300

    chegando a mesma parte de antes

---
+++ componentes/componente-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do usuário: <strong>{{inverterNome()}}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nomeUsuario: {
+            type: String,
+            //required: true,
+            default: "Anônimo"
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nomeUsuario.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            const antigo = this.nomeUsuario
+            this.nomeUsuario = 'Pedro'
+            this.$emit('nomeMudou', {antigo, novo: this.nomeUsuario})
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>

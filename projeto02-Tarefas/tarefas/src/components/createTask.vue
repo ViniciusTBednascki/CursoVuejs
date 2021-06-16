cee730c423ab5a5d8d6e19d2b865e6a7a5df92dc
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-16 16:48:48 -0300

    mudando o estilo do input das tarefas

--- projeto02-Tarefas/tarefas/src/components/createTask.vue
+++ projeto02-Tarefas/tarefas/src/components/createTask.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="taskInput">
-        <input type="text" v-model="taskName" @keyup.enter="addNewTask">
-        <button @click="addNewTask">+</button>
+        <input type="text" v-model="taskName" @keyup.enter="addNewTask" class="formElement" placeholder="nova tarefa?">
+        <button @click="addNewTask" class="formElement">+</button>
     </div>
 </template>
 
@@ -22,5 +22,30 @@ export default {
 </script>
 
 <style scoped>
+    .taskInput {
+        margin: 35px;
+    }
 
+    .formElement {
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid #FFF;
+        padding: 5px 10px 8px;
+        color: #FFF;
+    }
+
+    input.formElement {
+        width: 500px;
+        background-color: #FFF2;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+
+    }
+
+    button.formElement {
+        border-left: none;
+        background-color: #2196F3;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
 </style>

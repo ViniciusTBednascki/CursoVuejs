029de5d038cee2e542917e150a10a2b6e96f497f
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-17 17:25:25 -0300

    registrando diretivas localmente

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -7,11 +7,32 @@
 		<hr>
 		<p v-destaque.atrasar="'teal'">Usando diretiva personalizada</p>
 		<p v-destaque:fundo.atrasar="cor">Usando diretiva personalizada</p>
+		<hr>
+		<p v-destaque-local.atrasar="'teal'">Usando diretiva personalizada</p>
+		<p v-destaque-local:fundo.atrasar="cor">Usando diretiva personalizada</p>
 	</div>
 </template>
 
 <script>
 export default {
+	directives: {
+		'destaque-local': {
+			bind(el, binding) {
+				//el.style.backgroundColor = 'lightgreen'
+
+				let atraso = 0
+				if(binding.modifiers['atrasar']) { atraso = 3000 }
+
+				setTimeout(() => {
+					if(binding.arg === 'fundo') {
+						el.style.backgroundColor = binding.value
+					}else {
+						el.style.color = binding.value
+					}
+				}, atraso)
+			}
+		}
+	},
 	data() {
 		return {
 			cor: 'springgreen'

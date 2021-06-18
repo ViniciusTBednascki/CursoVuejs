20da5be5b0371fb2fa9fbd2609d8527e260d7fb5
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-18 15:51:27 -0300

    criando um filtro global que conta as letras de cada palavra na frase

--- filtros-mixins/filtros-mixins-desafio/src/App.vue
+++ filtros-mixins/filtros-mixins-desafio/src/App.vue
@@ -1,14 +1,15 @@
 <template>
 	<div id="app">
 		<h1>Filtros & Mixins (Desafio)</h1>
-		<p>{{frase | virgula}}</p>
 		<!-- Exercício 1 -->
 		<!-- Construir um filtro local que troca espaços por vírgula -->
-		
+		<p>{{frase | virgula}}</p>
+		<hr>
 		<!-- Exercício 2 -->
 		<!-- Filtro global que conta o tamanho de cada palavra e adiciona o 
 			valor na string final -->
 		<!-- "Pedro é legal" => "Pedro (5) é (1) legal (5)" -->
+		<p>{{frase | contaLetra}}</p>
 
 		<!-- Exercício 3 -->
 		<!-- Implementar os exercicios 1 e 2 com propriedade computada -->
@@ -27,7 +28,7 @@ export default {
 	},
 	data() {
 		return {
-			frase: "Testando o fitro de troca"
+			frase: "Testando os fitros"
 		}
 	}
 }
--- filtros-mixins/filtros-mixins-desafio/src/main.js
+++ filtros-mixins/filtros-mixins-desafio/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.filter('contaLetra', function(valor) {
+	const array = valor.split(' ')
+	array.forEach( (palavra, index) => {
+		array[index] = `${palavra} (${palavra.length})`
+	})
+	return array.join(' ')
+})
 new Vue({
 	render: h => h(App),
 }).$mount('#app')

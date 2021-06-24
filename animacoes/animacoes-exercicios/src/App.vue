569836366f76aa59450569b0117628e8df603406
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-24 14:13:46 -0300

    criando as funções de transição

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -26,6 +26,12 @@
 			<b-alert variant="info" show v-if="exibir" key="info">{{msg}}</b-alert>
 			<b-alert variant="warning" show v-else key="warn">{{msg}}</b-alert>
 		</transition>
+
+		<hr>
+		<b-button variant="success" @click="exibir2 = !exibir2">Mostrar</b-button>
+		<transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @enter-cancelled="enterCancelled" @before-leave="beforeLeave" @leave="leave" @after-leave="afterLeave" @leave-cancelled="leaveCancelled">
+			<div v-if="exibir2" class="caixa"></div>
+		</transition>
 	</div>
 </template>
 
@@ -36,9 +42,38 @@ export default {
 		return {
 			msg: 'Uma mensagem de informação para o usuário!',
 			exibir: false,
+			exibir2: true,
 			tipoAnimacao: 'fade'
 		}
-	}
+	},
+    methods: {
+        beforeEnter(el) {
+			console.log('beforeEnter')
+        },
+        enter(el) {
+			console.log('enter')
+			//done()
+        },
+        afterEnter(el) {
+			console.log('afterEnter')
+        },
+        enterCancelled(el) {
+			console.log('enterCancelled')
+        },
+        beforeLeave(el) {
+			console.log('beforeLeave')
+        },
+        leave(el) {
+			console.log('leave')
+			//done()
+        },
+        afterLeave(el) {
+			console.log('afterLeave')
+        },
+        leaveCancelled(el) {
+			console.log('leaveCancelled')
+        }
+    }
 }
 </script>
 
@@ -51,6 +86,13 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 	font-size: 1.5rem;
+}
+
+.caixa {
+	height: 100px;
+	width: 300px;
+	margin: 30px auto;
+	background-color: lightgreen;
 }
 
 .fade-enter, .fade-leave-to {

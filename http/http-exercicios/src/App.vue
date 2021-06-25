2ef30f9340af1c8aed06fec1fc4d3a7b8c7ad5c6
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-25 16:08:10 -0300

    adicionando mensagens de resposta as requisições

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-alert show dismissible v-for="mensagem in mensagens" :key="mensagem.texto" :variant="mensagem.tipo">{{mensagem.texto}}</b-alert>
 		<b-card>
 			<b-form-group label="Nome:">
 				<b-form-input type="text" size="lg" v-model="usuario.nome" placeholder="Informe o nome"></b-form-input>
@@ -29,6 +30,7 @@
 export default {
 	data() {
 		return {
+			mensagens: [],
 			usuarios: [],
 			id: null,
 			usuario: {
@@ -41,19 +43,33 @@ export default {
 		limpar() {
 			this.usuario.nome = '',
 			this.usuario.email = '',
-			this.id = null
+			this.id = null,
+			this.mensagens = []
 		},
 		carregar(id) {
 			this.id = id,
 			this.usuario = {...this.usuarios[id]}
 		},
 		excluir(id) {
-			this.$http.delete(`/usuarios/${id}.json`).then(() => {this.limpar()})
+			this.$http.delete(`/usuarios/${id}.json`).then(() => {
+				this.limpar()
+			}).catch(() => {
+				this.mensagens.push({
+					texto: "Problema para excluir!",
+					tipo: "danger"
+				})
+			})
 		},
 		salvar() {
 			const metodo = this.id? 'patch' : 'post'
 			const finalUrl = this.id? `/${this.id}.json` : '.json'
-			this.$http[metodo](`/usuarios${finalUrl}`,	this.usuario).then(() => this.limpar())
+			this.$http[metodo](`/usuarios${finalUrl}`,	this.usuario).then(() => {
+				this.limpar()
+				this.mensagens.push({
+					texto: "Operação realizada com sucesso!",
+					tipo: "success"
+				})
+				})
 		},
 		obterUsuarios() {
 			this.$http.get('usuarios.json').then(res => {

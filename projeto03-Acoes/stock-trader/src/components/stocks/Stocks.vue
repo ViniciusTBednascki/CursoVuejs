2ecf70f45360be569ad762afd2c7be79d39e94d7
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-07-09 15:56:21 -0300

    criando a rota Stocks e os componente Stock

--- projeto03-Acoes/stock-trader/src/components/stocks/Stocks.vue
+++ projeto03-Acoes/stock-trader/src/components/stocks/Stocks.vue
@@ -1,10 +1,24 @@
 <template>
-  <h1>Ações</h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"/>
+  </v-layout>
 </template>
 
 <script>
-export default {
+import Stock from './Stock.vue'
 
+export default {
+  components: {Stock},
+  data() {
+    return {
+      stocks: [
+        {id: 1, name: 'BMW', price: 110},
+        {id: 2, name: 'Google', price: 185},
+        {id: 3, name: 'Apple', price: 206},
+        {id: 4, name: 'Twitter', price: 87}
+      ]
+    }
+  }
 }
 </script>
 

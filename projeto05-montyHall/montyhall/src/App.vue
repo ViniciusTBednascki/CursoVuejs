f8147e39b2acc9689f09f964fa916b4a69e4af56
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-07-22 16:13:44 -0300

    arquivos iniciais do projeto05-montyHall

---
+++ projeto05-montyHall/montyhall/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>Monty Hall Problem</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  body {
+    color: whitesmoke;
+    background: linear-gradient(to right, #0f0c29, #302b63, #24243e);
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #app h1 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+  }
+</style>

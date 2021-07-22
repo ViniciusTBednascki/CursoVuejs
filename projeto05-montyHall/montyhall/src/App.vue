2e4ed66d6cb897596b8943aa9dbf55aafad3bead
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-07-22 16:36:53 -0300

    criando o componente Gift

--- projeto05-montyHall/montyhall/src/App.vue
+++ projeto05-montyHall/montyhall/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
     <h1>Monty Hall Problem</h1>
+    <Gift />
   </div>
 </template>
 
 <script>
+import Gift from './componentes/Gift.vue'
 export default {
-
+  name: 'App',
+  components: {Gift}
 }
 </script>
 

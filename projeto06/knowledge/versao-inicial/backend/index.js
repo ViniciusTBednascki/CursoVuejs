6424abbe968ed64dc92760693286d16ab654a301
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-07-29 17:20:48 -0300

    criando a parte inicial dos middlewares e configurando o consign

--- projeto06/knowledge/versao-inicial/backend/index.js
+++ projeto06/knowledge/versao-inicial/backend/index.js
@@ -1,4 +1,9 @@
 const app = require('express')()
+const consign = require('consign')
+
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
 
 const port = 3000
 app.listen(port, () => {

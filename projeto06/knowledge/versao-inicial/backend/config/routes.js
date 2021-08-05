8489a34088c36b992834357519bb34ff49624560
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-08-05 16:19:16 -0300

    implementando o método para receber a árvore de categorias

--- projeto06/knowledge/versao-inicial/backend/config/routes.js
+++ projeto06/knowledge/versao-inicial/backend/config/routes.js
@@ -11,6 +11,10 @@ module.exports = app => {
         .post(app.api.category.save)
         .get(app.api.category.get)
 
+    // cuidado com a ordem na qual as rotas são implementadas
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
+
     app.route('/categories/:id')
         .put(app.api.category.save)
         .get(app.api.category.getById)

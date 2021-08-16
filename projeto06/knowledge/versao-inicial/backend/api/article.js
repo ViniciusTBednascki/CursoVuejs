652e79f91db86828f76f31f60f63dad809a143b1
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-08-16 16:51:47 -0300

    criando os métodos get e getById para o controller dos artigos e implementando as rotas para os artigos

--- projeto06/knowledge/versao-inicial/backend/api/article.js
+++ projeto06/knowledge/versao-inicial/backend/api/article.js
@@ -1,16 +1,16 @@
 module.exports = app => {
-    const { existsOrError, notExistsOrError } = app.api.validate
+    const { existsOrError } = app.api.validate
 
     const save = (req, res) => {
         const article = { ...req.body }
         if(req.params.id) article.id = req.params.id
 
         try {
-            existsOrError(user.name, "Nome não foi informado")
-            existsOrError(user.description, "Descrição não foi informada")
-            existsOrError(user.categoryId, "Categoria não foi informada")
-            existsOrError(user.userId, "Autor não foi informado")
-            existsOrError(user.content, "Conteúdo não foi informado")
+            existsOrError(article.name, "Nome não foi informado")
+            existsOrError(article.description, "Descrição não foi informada")
+            existsOrError(article.categoryId, "Categoria não foi informada")
+            existsOrError(article.userId, "Autor não foi informado")
+            existsOrError(article.content, "Conteúdo não foi informado")
         } catch(msg) {
             return res.status(400).send(msg)
         }
@@ -30,11 +30,42 @@ module.exports = app => {
         try {
             const rowsDeleted = await app.db('articles')
                 .where({ id: req.params.id}).del()
-            existsOrError(rowsDeleted, "Artigo não foi encontrado")
-
+            
+            try {
+                existsOrError(rowsDeleted, "Artigo não foi encontrado")
+            } catch {
+                return res.status(400).send(msg)
+            }
+            
             res.status(204).send()
         } catch(msg) {
-            res.status(400).send(msg)
+            res.status(500).send(msg)
         }
     }
+
+    const get = async (req, res) => {
+        const limit = parseInt(req.query.limit) || 10 //usando para a paginação
+        const page = parseInt(req.query.page) || 1
+
+        const result =  await app.db('articles').count('id').first()
+        const count = parseInt(result.count)
+        
+        app.db('articles')
+            .select('id', 'name', 'description')
+            .limit(limit).offset(page * limit - limit)
+            .then(articles => res.json({ data: articles, count, limit }))
+            .catch(err => res.status(500).send(err))
+    }
+
+    const getById = (req, res) => {
+        app.db('articles')
+            .where({ id: req.params.id }).first()
+            .then(article => {
+                article.content = article.content.toString()
+                return res.json(article)
+            })
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { save, remove, get, getById }
 }

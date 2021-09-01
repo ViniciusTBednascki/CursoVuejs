1121bba3f58b35e098731bf404b375bfd611cf91
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-09-01 15:47:10 -0300

    adicionando validação para criação de administradores

--- projeto06/knowledge/versao-inicial/backend/api/user.js
+++ projeto06/knowledge/versao-inicial/backend/api/user.js
@@ -11,6 +11,9 @@ module.exports = app => {
         const user = { ...req.body }
         if(req.params.id) user.id = req.params.id
 
+        if(!req.originalUrl.startsWith('/users')) user.admin = false
+        if(!req.user || !req.user.admin) user.admin = false
+
         try {
             existsOrError(user.name, "Nome não foi informado")
             existsOrError(user.email, "E-mail não foi informado")

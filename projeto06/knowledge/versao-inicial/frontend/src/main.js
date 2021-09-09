14b55b23f4b6024b1eb3a2fcb006facd524657cd
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-09-09 17:53:33 -0300

    Criando o componente Stat e implementando o axios para conenctar ao backend

--- projeto06/knowledge/versao-inicial/frontend/src/main.js
+++ projeto06/knowledge/versao-inicial/frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+//TEMPORARIO!!!!!!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MywibmFtZSI6ImZ1bGFubyIsImVtYWlsIjoiZnVsYW5vQGVtYWlsLmNvbSIsImFkbWluIjpmYWxzZSwiaWF0IjoxNjMxMjIwMDI4LCJleHB0IjoxNjMxMzA2NDI4fQ.pLqeuh_V1ewax_XSvtyQKQgmNN9BDdVJmWBk7KwQ5Qk'
+
 new Vue({
   store,
   router,

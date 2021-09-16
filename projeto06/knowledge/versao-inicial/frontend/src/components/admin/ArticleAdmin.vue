5775ee308ce0c022938312c55f1b830c66ab0b0d
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-09-16 17:58:33 -0300

    criando o início do cadastro de artigos

--- projeto06/knowledge/versao-inicial/frontend/src/components/admin/ArticleAdmin.vue
+++ projeto06/knowledge/versao-inicial/frontend/src/components/admin/ArticleAdmin.vue
@@ -1,12 +1,116 @@
 <template>
   <div class="article-admin">
-      <h1>Artigos Componente</h1>
+      <b-form>
+        <input id="article-id" type="hidden" v-model="article.id">
+        <b-form-group label="Nome:" label-for="article-name">
+          <b-form-input id="article-name" type="text" v-model="article.name"
+            required :readonly="mode === 'remove'"
+            placeholder="Informe o Nome do Artigo..."/>
+        </b-form-group>
+        <b-form-group label="Descrição:" label-for="article-description">
+          <b-form-input id="article-description" type="text" v-model="article.description"
+            required :readonly="mode === 'remove'"
+            placeholder="Informe a Descrição do Artigo..."/>
+        </b-form-group>
+        <b-form-group label="Imagem (URL):" label-for="article-imageUrl">
+          <b-form-input id="article-imageUrl" type="text" v-model="article.imageUrl"
+            required :readonly="mode === 'remove'"
+            placeholder="Informe a URL da Imagem..."/>
+        </b-form-group>
+        <b-form-group v-if="mode == 'save'" label="Categoria:" label-for="article-categoryId">
+          <b-form-select id="article-categoryId" v-model="article.categoryId" :options="categories"/>
+        </b-form-group>
+        <b-form-group v-if="mode == 'save'" label="Autor:" label-for="article-userId">
+          <b-form-select id="article-userId" v-model="article.userId" :options="users"/>
+        </b-form-group>
+        <b-form-group label="Conteúdo:" label-for="article-content">
+          <VueEditor v-model="article.content" placeholder="Informe o Conteúdo do Artigo..."/>
+        </b-form-group>
+        <b-button variant="primary" v-if="mode === 'save'"
+          @click="save">Salvar</b-button>
+        <b-button variant="danger" v-if="mode === 'remove'"
+          @click="remove">Excluir</b-button>
+        <b-button class="ml-2" @click="reset">Cancelar</b-button>
+      </b-form>
+      <hr>
+      <b-table hover striped :items="articles" :fields="fields">
+        <template slot="cell(actions)" slot-scope="data">
+          <b-button variant="warning" @click="loadArticle(data.item)" class="mr-2">
+            <i class="fa fa-pencil"/>
+          </b-button>
+          <b-button variant="danger" @click="loadArticle(data.item, 'remove')">
+            <i class="fa fa-trash"/>
+          </b-button>
+        </template>
+      </b-table>
   </div>
 </template>
 
 <script>
+import { VueEditor } from 'vue2-editor'
+import axios from 'axios'
+import { baseApiUrl, showError } from '@/global'
+
 export default {
-    name: 'ArticleAdmin'
+    name: 'ArticleAdmin',
+    components: { VueEditor },
+    data() {
+      return {
+        mode: 'save',
+        article: {},
+        articles: [],
+        categories: [],
+        users: [],
+        page: 1,
+        limit: 0,
+        count: 0,
+        fields: [
+          { key: 'id', label: 'Código', sortable: true },
+          { key: 'name', label: 'Nome', sortable: true },
+          { key: 'description', label: 'Descrição', sortable: true },
+          { key: 'actions', label: 'Ações' }
+        ]
+      }
+    },
+    methods: {
+      loadArticles() {
+        const url = `${baseApiUrl}/articles`
+        axios.get(url).then(res => {
+          this.articles = res.data
+        })
+      },
+      reset() {
+        this.mode = 'save'
+        this.article = {}
+        this.loadArticles()
+      },
+      save() {
+        const method = this.article.id ? 'put' : 'post'
+        const id = this.article.id ? `/${this.article.id}` : ''
+        axios[method](`${baseApiUrl}/articles${id}`, this.article)
+          .then(() => {
+            this.$toasted.global.defaultSuccess()
+            this.reset()
+          })
+          .catch(showError)
+      },
+      remove() {
+        const id = this.article.id
+        axios.delete(`${baseApiUrl}/articles/${id}`)
+          .then(() => {
+            this.$toasted.global.defaultSuccess()
+            this.reset()
+          })
+          .catch(showError)
+      },
+      loadArticle(article, mode = 'save') {
+        this.mode = mode
+        this.article = { ...article }
+      }
+    },
+    mounted() {
+      this.loadArticles()
+    }
 }
 </script>
 
